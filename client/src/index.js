24248cb57fb372c64ef874168b72bc3a21ccce09
victor-tuda <victor_gtuda@outlook.com>
2023-03-08 13:34:45 -0300

    Adicionado um background na pagina inicial

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import './index.css';
+
 
 import reducers from './reducers';
 
