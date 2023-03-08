83c3ba223579b9aca5580d557d6a842cedd9e57c
victor-tuda <victor_gtuda@outlook.com>
2023-03-08 14:42:42 -0300

    Bug update cards

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -26,4 +27,15 @@ export const createPost = async (req, res) => {
     catch (error) {
         res.status(409).json({message: message.error});
     }
+}
+
+export const updatePost = async (req, resp) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true})
+
+    res.json(updatedPost);
 }
