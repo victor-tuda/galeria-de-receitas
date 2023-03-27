935373f37a889722f50415d3abebb058d9d9c7af
victor-tuda <victor_gtuda@outlook.com>
2023-03-27 14:18:06 -0300

    Fix cards cores e fontes

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -31,7 +31,7 @@ const Post = ({ post, setCurrentId }) => {
             </div>
                 <Typography className={classes.title} variant="h5" gutterButton>{post.title}</Typography>
             <CardContent>
-                <Typography variant="h10" gutterButton>{post.message}</Typography>
+                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
